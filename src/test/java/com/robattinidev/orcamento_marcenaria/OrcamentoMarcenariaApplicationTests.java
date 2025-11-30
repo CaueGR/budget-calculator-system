6dfc5d10167c106a7e2df7a8dfd5f807ef3c9cb1
Caue Robattini <cauegr18@gmail.com>
2025-11-30 19:18:06 -0300

    feat: initial project structure with Spring Boot and dependencies

---
+++ src/test/java/com/robattinidev/orcamento_marcenaria/OrcamentoMarcenariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robattinidev.orcamento_marcenaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoMarcenariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
